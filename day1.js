bb49dc7a8a691f88e09439a67510fabee47a8142
Shwetha-Iyer <shwetharaj555@gmail.com>
2021-06-07 17:11:29 +0530

    Delete day1.js

--- day1.js
+++
@@ -1,8 +0,0 @@
-const {add,subtract,multiply,divide,modulus} = require("./calculator");
-var a=10;
-var b=8;
-console.log(add(a,b));
-console.log(subtract(a,b));
-console.log(multiply(a,b));
-console.log(divide(a,b));
-console.log(modulus(a,b));
